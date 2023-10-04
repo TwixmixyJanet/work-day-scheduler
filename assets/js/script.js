42b15002fc3d237fca15b4114cd703a3e9b9e163
Janet Webster <twixmixy@gmail.com>
2023-10-04 19:28:34 -0400

    Making comments in the JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// ~~~ TODAY'S DATE ~~~ //
+//(document).ready <--may need to be used if other jQuery/JS are in the script document
+// Initiate the function for all other code.
+$(function() {
+  // ~~~ TODAY'S DATE ~~~ //
+  // When researching this I got confused because I saw articles recommending to use moment() instead of dayjs(), then I recalled we discussed moment yesterday and how it's basically no longer used. Also the fact that our starter docs gave us an API to dayjs.
+  // The format I based off of the visual in the assignment gif image. Researched on jarte.com to see different display options.
+  // Added a bit of fun to set an internal to count the time for each second
+  setInterval(function() {
+  var todaysDate = dayjs().format('dddd, MMM D YYYY h:mm:ssA');
+  // I'm struggling to adjust to jquery. I didn't see a difference between using .text or .html, so I went with .html
+  $("#currentDay").html(todaysDate);
+  console.log(todaysDate);
+}, 1000)
 
-// 
-$(document).ready(function () {
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // ~~~ SAVE to LOCAL STORAGE ~~~ //
+  // save button class called, using jquery .on to initiate the event listener of "click", initiating the function.
     $(".saveBtn").on("click", function() {
+      // Establish variables to hold text and time data
+      // (this) refers it's attachment to the save button
+      // .sibling and .parent are the relationship to the button
+      // .sibling is connecting to the .description class element
+      // .parent is connecting to the id attribute, no matter what that id is labeled
+      // val() is explicitly getting the value of the text area element
       var textArea = $(this).siblings(".description").val();
       var timeSaved = $(this).parent().attr("id");
 
+      // localStorage is setting the time and text of when the save button is clicked.
       localStorage.setItem(timeSaved, textArea);
     });
 
@@ -25,9 +35,12 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  // ~~~ TIME TRACKING ~~~ //
   function timeTracking() {
+    // using the dayjs setting a variable to explicitly pull the hour
     var currentTime = dayjs().hour();
     console.log("Current Hour: " + currentTime); 
+    
     $(".time-block").each(function() {
       var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
 
@@ -57,6 +70,8 @@ $(document).ready(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+    // ~~~ RETRIEVE FROM LOCAL STORAGE ~~~ //
+
     $("#hour-6 .description").val(localStorage.getItem("hour-6"));
     $("#hour-7 .description").val(localStorage.getItem("hour-7"));
     $("#hour-8 .description").val(localStorage.getItem("hour-8"));
@@ -72,13 +87,7 @@ $(document).ready(function () {
     $("#hour-18 .description").val(localStorage.getItem("hour-18"));
 
 
-  // ~~~ TODAY'S DATE ~~~ //
-  // When researching this I got confused because I saw articles recommending to use moment() instead of dayjs(), then I recalled we discussed moment yesterday and how it's basically no longer used. Also the fact that our starter docs gave us an API to dayjs.
-  // The format I based off of the visual in the assignment gif image. Researched on jarte.com to see different display options.
-  var todaysDate = dayjs().format('dddd, MMM D YYYY');
-  // I'm struggling to adjust to jquery. I didn't see a difference between using .text or .html, so I went with .html
-  $("#currentDay").html(todaysDate);
-  console.log(todaysDate);
+
 
   // Calling on the time.Tracking function.
   timeTracking();
